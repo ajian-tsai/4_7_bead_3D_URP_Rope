a3131bbce49679ee1a09a4c3fed7c7341e21643e
ajian-tsai <chiufocal520@gmail.com>
2023-07-08 11:59:39 +0800

    線條加長

---
+++ 0407_bead_rope2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	[SerializeField] private float speed = 1;
+	ObiRopeCursor cursor;
+	ObiRope rope;
+
+	void Start()
+	{
+		cursor = GetComponentInChildren<ObiRopeCursor>();
+		rope = cursor.GetComponent<ObiRope>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Z))
+			cursor.ChangeLength(rope.restLength - speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.X))
+			cursor.ChangeLength(rope.restLength + speed * Time.deltaTime);
+	}
+}
